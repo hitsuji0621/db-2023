9c715078eeca396897afe2eb7f33936d0ed11620
hitsuji0621 <fatsheep0621@gmail.com>
2023-06-07 20:58:16 +0800

    init

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route("/")
+def hello():
+    return render_template("hero.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+
+
+app.run(host="0.0.0.0")
