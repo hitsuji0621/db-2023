afc62e54ce27eb471381c49a683418ae07da35c0
Scout1208 <yuuuyy5@gmail.com>
2023-06-13 15:56:32 +0800

    change name

--- main_database.py
+++ main_database.py
@@ -18,12 +18,26 @@ config = {
 def index():
     connection = mysql.connector.connect(**config)
     cursor = connection.cursor()
+    #
+    # # 讀取 SQL 檔案
+    # with open('final.sql', 'r') as file:
+    #     sql_statements = file.read()
+    #
+    # # 以分號為分隔符號拆分指令
+    # statements = sql_statements.split(';')
+    #
+    # # 執行每個 SQL 指令
+    # for statement in statements:
+    #     if statement.strip() != '':
+    #         cursor.execute(statement)
+    # 提交事務
+    connection.commit()
 
     # 關閉資料庫連接
     cursor.close()
     connection.close()
 
-    return "連上啦"
+    return "DDL 已匯入到資料庫"
 
 if __name__ == '__main__':
     app.run()
