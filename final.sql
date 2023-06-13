5ac0ab793f26088c2a108ca7fa420b03e01cc430
Scout1208 <yuuuyy5@gmail.com>
2023-06-13 15:13:40 +0800

    update ddl

--- final.sql
+++ final.sql
@@ -1,6 +1,6 @@
 create table applicant
 (
-    applicant_id    varchar(20) primary key AUTO_INCREMENT,
+    applicant_id    varchar(20) primary key,
     email           varchar(20),
     password        varchar(20),
     zh_name         varchar(20),
@@ -13,45 +13,37 @@ create table applicant
 
 create table resume
 (
-    resume_id       varchar(20) primary key AUTO_INCREMENT,
+    resume_id       varchar(20) primary key,
     file_name       varchar(20),
     file_path       varchar(20)
-)ENGINE=INNODB;
-create table apply
-(
-    applylicant_id  varchar(20) primary key AUTO_INCREMENT,
-    job_id          varchar(20),
-    date            varchar(20),
-    resume_id       varchar(20),
-    state           varchar(20)
-    foreign key (resume_id) references resume(resume_id)
 )ENGINE=INNODB;
 
 create table company
 (
-    company_id          varchar(20) primary key AUTO_INCREMENT,
+    company_id          varchar(20) primary key,
     company_name        varchar(20),
     address             varchar(20),
     phone               varchar(20),
     website             varchar(20),
     category            varchar(20)
-)ENGINE=INNODB;
+) ENGINE=INNODB;
 
 create table job
 (
-    job_id              varchar(20) primary key AUTO_INCREMENT,
-    job_name            varchar(20) ,
-    company             varchar(20) ,
-    requirement         varchar(20) ,
-    category            varchar(20) ,
+    job_id              varchar(20) primary key,
+    job_name            varchar(20),
+    company_id          varchar(20),
+    requirement         varchar(20),
+    category            varchar(20),
     description         varchar(100),
-    foreign key (company) references company(company_name)
-)ENGINE=INNODB;
+    foreign key ( company_id) references company( company_id)
+) ENGINE=INNODB;
+
 
 
 create table events
 (
-    event_id        varchar(20) primary key AUTO_INCREMENT,
+    event_id        varchar(20) primary key,
     event_name      varchar(20),
     website         varchar(20),
     category        varchar(20),
@@ -62,7 +54,7 @@ create table events
 
 create table admin
 (
-    admin_id        varchar(20) primary key AUTO_INCREMENT,
+    admin_id        varchar(20) primary key,
     password        varchar(20),
     name            varchar(20),
     hash_value      varchar(20),
@@ -72,7 +64,7 @@ create table admin
 
 create table hire
 (
-    applicant_id    varchar(20) primary key AUTO_INCREMENT,
+    applicant_id    varchar(20) primary key,
     company_id      varchar(20),
     foreign key (applicant_id) references applicant(applicant_id),
     foreign key (company_id) references company(company_id)
@@ -80,17 +72,19 @@ create table hire
 
 create table apply
 (
-    applicant_id    varchar(20) primary key AUTO_INCREMENT,
+    applicant_id    varchar(20) primary key,
     job_id      varchar(20),
     data        DATETIME,
+    resume_id   varchar(20),
     state       varchar(20),
     foreign key (applicant_id) references applicant(applicant_id),
-    foreign key (job_id) references job(job_id)
+    foreign key (job_id) references job(job_id),
+    foreign key (resume_id) references resume(resume_id)
 )ENGINE=INNODB;
 
 create table participate
 (
-    event_id    varchar(20) primary key AUTO_INCREMENT,
+    event_id    varchar(20) primary key,
     applicant_id   varchar(20),
     foreign key (applicant_id) references applicant(applicant_id),
     foreign key (event_id) references events(event_id)
@@ -98,13 +92,13 @@ create table participate
 
 create table submit
 (
-    job_id  varchar(20),
+    job_id  varchar(20) primary key,
     resume_id varchar(20),
     foreign key (job_id) references job(job_id)
 )ENGINE=INNODB;
 
 create table sponse
 (
-    event_id  varchar(20),
+    event_id  varchar(20) primary key,
     company_id  varchar(20)
 )ENGINE=INNODB;
