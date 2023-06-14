962119515e804ea36faf63b60bec6e7c84683d7a
BlackPrincess <75473812+Dennis1209@users.noreply.github.com>
2023-06-14 09:01:34 +0800

    base_nav, front_page, modify data
    加了nav bar和首頁，以及很爛的修改網頁

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from datetime import date
 
 app = Flask(__name__)
 
+
 # init_db
 app.config['SQLALCHEMY_DATABASE_URI'] = \
     f'mysql+pymysql://{db_config.username}:{db_config.password}@{db_config.hostname}:{db_config.port}/{db_config.db}'
@@ -15,6 +16,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = \
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = False
 db=SQLAlchemy(app)
+
 
 
 with app.app_context():
@@ -33,39 +35,49 @@ with app.app_context():
         'job': Base.classes.job
     }
 
-
     db_session = Session(db.engine, future=True)
+
 
 @app.route("/")
 def home():
     return render_template("hero.html")
 
+
 def db_add_user(form: dict):
-    birthday=form['birthday'] if form['birthday']!='' else str(date.today())
-    name=form['name']
-    phone=form['phone']
-    gender=form['gender']
-    email=form['email']
-    pwd=form['password']
+    birthday = form['birthday'] if form['birthday'] != '' else str(date.today())
+    name = form['name']
+    phone = form['phone']
+    gender = form['gender']
+    email = form['email']
+    pwd = form['password']
     db.session.add(db_table['applicant'](
         applicant_id=name, email=email, password=pwd, zh_name=name, phone=phone, gender=gender, birthday=birthday))
     db.session.commit()
 
+
 @app.route("/register", methods=['GET', 'POST'])
 def register():
-    if request.method=='POST':
+    if request.method == 'POST':
         print(request.form)
         db_add_user(request.form)
 
         return redirect(url_for('home'))
     else:
         return render_template("register.html")
-    
+
 
 @app.route("/login")
 def login():
     return render_template("login.html")
 
+@app.route("/front_page")
+def front_page():
+    return render_template("front_page.html")
 
-if __name__=='__main__':
-    app.run(host="0.0.0.0")
+@app.route("/modify_data")
+def modify_data():
+    return render_template("modify_data.html")
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",debug=True)
