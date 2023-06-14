d25d5b1ed519938975dcdab94a4c122dbeb872b1
kai <40947038S@ntnu.edu.tw>
2023-06-14 21:10:50 +0800

    test data

---
+++ test.sql
@@ -0,0 +1,7 @@
+insert company values (1, 'Google', 'Taiwan', '000', 'google.com', 'IT');
+insert company values (2, 'Baidu', 'China', '86', 'baidu.com', 'IT');
+insert job values (1, 'CEO', 1, 'God of the world', 'IT', 'Lead Google');
+insert job values (2, 'SWE', 1, '20 years-old but have 30 years experience', 'IT', 'Do any thing if Google wants');
+insert job values (3, 'SWE', 2, '996', 'IT', 'Do any thing if Xi wants');
+insert events values (1, 'computers give away', 'intel.com', 'give away', 'free AMD computer', '2023-06-04 10:30:00', '2023-06-09 10:30:00');
+insert events values (2, 'The Great Leap', 'baidu.com', 'revolution', 'IDK', '1957-12-31 10:30:00', '1960-01-01 10:30:00');
